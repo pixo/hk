7671a69f732c5932234f09d4f9fc6101a83fae98
pixo <pixo@destiny.(none)>
2013-12-27 21:51:30 +0100

    fixe environment file

--- python/assetmanager/0.1.0/badass/core/project.py
+++ python/assetmanager/0.1.0/badass/core/project.py
@@ -168,58 +168,49 @@ def createProjectEnv ( name = "" ):
         return False
 
     # TODO:Create this document with a ui
-    env_data = r"""source $HOME/.bashrc
-source $HK_ROOT/users/$USER/.hk/$HK_PROJECT
-
-#Set environment variables
-export HK_DB=$HK_PROJECT
-
-export HK_COAT_VER=4-0-03
-export HK_COAT_VER_T=4-0-03
-export HK_GUERILLA_VER=0.17.0b12
-export HK_GUERILLA_VER_T=0.17.0b12
-export HK_HIERO_VER=1.6v1
-export HK_HIERO_PLAYER_VER=1.6v1
-export HK_HOUDINI_VER=12.5
-export HK_MARI_VER=2.0v1
-export HK_MARI_VER_T=2.1v1a1
-export HK_MAYA_VER=2013
-export HK_MAYA_ROOT="/usr/autodesk"
-export HK_MODO_VER=701
-export HK_MUDBOX_VER=2013
-export HK_NUKE_VER=7.0v2
-export HK_BADASS_VER=%s
-
-if $HK_DEV_MODE
-    then
-        devmode="DEV-"
-fi
-
-alias ~~='cd $HK_HOME'
-
-logpath="$HK_USER_REPO"
-export HK_BADASS="$HK_CODE_PATH/python/assetmanagebadassPELINE_VER"
-export HK_COUCHDB="$HK_CODE_PATH/python/couchdb-python"
-export HK_PYSIDE="$HK_CODE_PATH/python/pyside"
-argparse="$HK_CODE_PATH/python/argparse"
-json="$HK_CODE_PATH/python/json"
-export PYTHONPATH="$HK_BADASS:$json:$argparse:$PYTHONPATH"
-
-#Set PS1
-export PS1='\[\033[1;34m\]|\u@\h\[\033[1;37m\]|\t\[\033[1;31m\]|$devmode$HK_PROJECT>\[\033[0;33m\]\w$ \[\033[00m\]'
-
-#Set the current directory
-if ! ([ -d $logpath ])
-      then
-        mkdir -p $logpath
-fi
-""" % utils.getBadassVersion ()
+    env_data = "source $HOME/.bashrc\n"
+    env_data += "source $HK_ROOT/users/$USER/.hk/$HK_PROJECT\n\n"
+    env_data += "#Set environment variables\n"
+    env_data += "export HK_DB=$HK_PROJECT\n"
+    env_data += "export HK_COAT_VER=4-0-03\n"
+    env_data += "export HK_COAT_VER_T=4-0-03\n"
+    env_data += "export HK_GUERILLA_VER=0.17.0b12\n"
+    env_data += "export HK_GUERILLA_VER_T=0.17.0b12\n"
+    env_data += "export HK_HIERO_VER=1.6v1\n"
+    env_data += "export HK_HIERO_PLAYER_VER=1.6v1\n"
+    env_data += "export HK_HOUDINI_VER=12.5\n"
+    env_data += "export HK_MARI_VER=2.0v1\n"
+    env_data += "export HK_MARI_VER_T=2.1v1a1\n"
+    env_data += "export HK_MAYA_ROOT=/usr/autodesk\n"
+    env_data += "export HK_MAYA_VER=2013\n"
+    env_data += "export HK_MODO_VER=701\n"
+    env_data += "export HK_MUDBOX_VER=2013\n"
+    env_data += "export HK_NUKE_VER=7.0v2\n"
+    env_data += "export HK_BADASS_VER=%s\n\n" % utils.getBadassVersion ()
+    env_data += "if $HK_DEV_MODE\n"
+    env_data += "    then\n"
+    env_data += "        hkmode=\"dev\"\n"
+    env_data += "fi\n\n"
+    env_data += "alias work='cd $HK_HOME'\n\n"
+    env_data += "logpath=\"$HK_USER_REPO\"\n"
+    env_data += "export HK_BADASS=\"$HK_CODE_PATH/python/assetmanager/$HK_BADASS_VER\"\n"
+    env_data += "export HK_COUCHDB=\"$HK_CODE_PATH/python/couchdb-python\"\n"
+    env_data += "export HK_PYSIDE=\"$HK_CODE_PATH/python/pyside\"\n"
+    env_data += "argparse=\"$HK_CODE_PATH/python/argparse\"\n"
+    env_data += "json=\"$HK_CODE_PATH/python/json\"\n"
+    env_data += "export PYTHONPATH=\"$HK_BADASS:$json:$argparse:$PYTHONPATH\"\n\n"
+    env_data += "#Set PS1\n"
+    env_data += r"export PS1='\[\033[1;34m\]|\u@\h\[\033[1;37m\]|\t\[\033[1;31m\]|$HK_PROJECT|$hkmode>\[\033[0;33m\]\w$ \[\033[00m\]'" + "\n\n"
+    env_data += "#Set the current directory\n"
+    env_data += "if ! ([ -d $logpath ])\n"
+    env_data += "      then\n"
+    env_data += "        mkdir -p $logpath\n"
+    env_data += "fi\n"
 
     # Get the project env file
     env_file = utils.getProjectEnv ( name )
         
     if env_file :
-        
         # Create Environment file
         if utils.createFile ( env_file, env_data ):
             print "createProjectEnv(): %s created " % env_file
--- python/assetmanager/0.1.0/badass/examples/examples.py
+++ python/assetmanager/0.1.0/badass/examples/examples.py
@@ -37,4 +37,46 @@ def getFileTexType ():
     result = core.getTextureAttr ( path )
     print result
 
-# getFileTexType ()
+def getEnv ():
+    env_data = "source $HOME/.bashrc\n"
+    env_data += "source $HK_ROOT/users/$USER/.hk/$HK_PROJECT\n\n"
+    env_data += "#Set environment variables\n"
+    env_data += "export HK_DB=$HK_PROJECT\n"
+    env_data += "export HK_COAT_VER=4-0-03\n"
+    env_data += "export HK_COAT_VER_T=4-0-03\n"
+    env_data += "export HK_GUERILLA_VER=0.17.0b12\n"
+    env_data += "export HK_GUERILLA_VER_T=0.17.0b12\n"
+    env_data += "export HK_HIERO_VER=1.6v1\n"
+    env_data += "export HK_HIERO_PLAYER_VER=1.6v1\n"
+    env_data += "export HK_HOUDINI_VER=12.5\n"
+    env_data += "export HK_MARI_VER=2.0v1\n"
+    env_data += "export HK_MARI_VER_T=2.1v1a1\n"
+    env_data += "export HK_MAYA_ROOT=/usr/autodesk\n"
+    env_data += "export HK_MAYA_VER=2013\n"
+    env_data += "export HK_MODO_VER=701\n"
+    env_data += "export HK_MUDBOX_VER=2013\n"
+    env_data += "export HK_NUKE_VER=7.0v2\n"
+    env_data += "export HK_BADASS_VER=%s\n\n" % utils.getBadassVersion ()
+    env_data += "if $HK_DEV_MODE\n"
+    env_data += "    then\n"
+    env_data += "        hkmode=\"dev\"\n"
+    env_data += "fi\n\n"
+    env_data += "alias work='cd $HK_HOME'\n\n"
+    env_data += "logpath=\"$HK_USER_REPO\"\n"
+    env_data += "export HK_BADASS=\"$HK_CODE_PATH/python/assetmanager/$HK_BADASS_VER\"\n"
+    env_data += "export HK_COUCHDB=\"$HK_CODE_PATH/python/couchdb-python\"\n"
+    env_data += "export HK_PYSIDE=\"$HK_CODE_PATH/python/pyside\"\n"
+    env_data += "argparse=\"$HK_CODE_PATH/python/argparse\"\n"
+    env_data += "json=\"$HK_CODE_PATH/python/json\"\n"
+    env_data += "export PYTHONPATH=\"$HK_BADASS:$json:$argparse:$PYTHONPATH\"\n\n"
+    env_data += "#Set PS1\n"
+    env_data += r"export PS1='\[\033[1;34m\]|\u@\h\[\033[1;37m\]|\t\[\033[1;31m\]|$HK_PROJECT|$hkmode>\[\033[0;33m\]\w$ \[\033[00m\]'" + "\n\n"
+    env_data += "#Set the current directory\n"
+    env_data += "if ! ([ -d $logpath ])\n"
+    env_data += "      then\n"
+    env_data += "        mkdir -p $logpath\n"
+    env_data += "fi\n"
+
+    return env_data
+    
+# getEnv ()
